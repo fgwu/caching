f0260edb5ccb395b901b429523505a363082bf11
Ravi Singh <singh415@umn.edu>
2019-06-28 13:27:52 -0500

    Updating the names of the logs so that the results are not overwritten and adding a variable that makes editting the size of the db much easier

--- script/vary_cache_size_randread.sh
+++ script/vary_cache_size_randread.sh
@@ -2,14 +2,15 @@
 
 #testing different cache sizes and getting the read random results
 
-./db_bench --benchmarks="fillrandom" -num=200000000 -use_direct_reads=true
+num_entries=200000000
+./db_bench --benchmarks="fillrandom" -num=$num_entries -use_direct_reads=true
 :>result_read.log
 
 for size_of_cache in  20000000 200000000 2000000000;
 do
-   ./db_bench --benchmarks="readrandom" -use_existing_db=true -use_direct_reads=True -cache_size ${size_of_cache} -statistics > tmp.log
-   cat tmp.log | grep micros/op >> result_read.log
-   cat tmp.log | grep rocksdb.block.cache.miss >> result_read.log
-   cat tmp.log | grep rocksdb.block.cache.hit >> result_read.log
-   cat tmp.log | grep rocksdb.block.cache.add >> result_read.log
+   ./db_bench --benchmarks="readrandom" -num=$num_entries -use_existing_db=true -use_direct_reads=True -cache_size ${size_of_cache} -statistics > randread_cache_size_${size_of_cahce}.log
+   cat randread_cache_size_${size_of_cache}.log | grep micros/op >> result_read.log
+   cat randread_cache_size_${size_of_cache}.log | grep rocksdb.block.cache.miss >> result_read.log
+   cat randread_cache_size_${size_of_cache}.log | grep rocksdb.block.cache.hit >> result_read.log
+   cat randread_cache_size_${size_of_cache}.log | grep rocksdb.block.cache.add >> result_read.log
 done
--- script/vary_cache_size_randseek.sh
+++ script/vary_cache_size_randseek.sh
@@ -2,14 +2,16 @@
 
 #testing different cache sizes and getting the random seek results
 
-./db_bench --benchmarks="fillrandom" -num=200000000 -use_direct_reads=true
+num_entries=200000000
+echo $num_entries
+./db_bench --benchmarks="fillrandom" -num=${num_entries} -use_direct_reads=true
 :>result_seek.log
 
 for size_of_cache in 20000000 200000000 2000000000;
 do
-   ./db_bench --benchmarks="seekrandom" -use_existing_db=true -use_direct_reads=True -cache_size ${size_of_cache} -statistics > tmp.log
-   cat tmp.log | grep micros/op >> result_seek.log
-   cat tmp.log | grep rocksdb.block.cache.miss >> result_seek.log
-   cat tmp.log | grep rocksdb.block.cache.hit >> result_seek.log
-   cat tmp.log | grep rocksdb.block.cache.add >> result_seek.log
+   ./db_bench --benchmarks="seekrandom" -num=${num_entries} -use_existing_db=true -use_direct_reads=True -cache_size ${size_of_cache} -statistics > randseek_cache_size_${size_of_cahce}.log
+   cat randseek_cache_size_${size_of_cache}.log | grep micros/op >> result_seek.log
+   cat randseek_cache_size_${size_of_cache}.log | grep rocksdb.block.cache.miss >> result_seek.log
+   cat randseek_cache_size_${size_of_cache}.log | grep rocksdb.block.cache.hit >> result_seek.log
+   cat randseek_cache_size_${size_of_cache}.log | grep rocksdb.block.cache.add >> result_seek.log
 done
